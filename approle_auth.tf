4975119e0bd9222c0fc36caff362ae80285dbed3
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-05-26 16:27:38 +0200

    feat: change approle path to <prefix>/approle instead of <prefix>-approle

--- approle_auth.tf
+++ approle_auth.tf
@@ -1,6 +1,6 @@
 resource "vault_auth_backend" "approle" {
   type = "approle"
-  path = "${var.tenant_prefix}-approle"
+  path = "${var.tenant_prefix}/approle"
   tune {
     default_lease_ttl = "3600s"
     max_lease_ttl     = "14400s"
